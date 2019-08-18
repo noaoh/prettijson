c3a68f4c94d633d730f2094e6fb56e7c17cac9a4
Noah Holt <noahryanholt@gmail.com>
2019-08-18 11:30:26 -0500

    Added newline characters to messages

--- prettijson/prettijson.py
+++ prettijson/prettijson.py
@@ -4,49 +4,53 @@ import logging
 import azure.functions as func
 
 def prettijson(data):
-    return json.dumps(data, indent=2, sort_keys=True, separators=(", ", " : "), check_circular=True)
+    seps = (", ", " : ")
+    return (
+        json.dumps(data, indent=2, sort_keys=True, separators=seps) + "\n"
+    )
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
+    logging.info("Python HTTP trigger function processed a request\n")
     if req.method == "POST":
         logging.info("Handling POST web request")
         if req.headers["Content-Type"] != "application/json":
             logging.error("Request did not have proper Content-Type")
             return func.HttpResponse(
-                "Content-Type header must be set to 'application/json'",
+                "Content-Type header must be set to 'application/json'\n",
                 status_code=400
             )
         
-        logging.info("Attempting to prettify JSON")
+        logging.info("Attempting to prettify JSON\n")
         try:
             j = req.get_json()
             prettified_json = prettijson(j)
         except Exception as e:
-            logging.error(f"Request was invalid JSON: {e}")
+            logging.error(f"Request was invalid JSON: {e}\n"])
             return func.HttpResponse(
-                "Request was invalid JSON",
+                "Request was invalid JSON\n",
                 status_code=400
             )
         
-        logging.info("Successfully prettified JSON")
+        logging.info("Successfully prettified JSON\n")
         return func.HttpResponse(
             prettified_json,
             status_code=200
         )
 
     elif req.method == "OPTIONS":
-        logging.info("Handling OPTIONS web request")
+        logging.info("Handling OPTIONS web request\n")
         return func.HttpResponse(
             json.dumps(["OPTIONS", "POST"]),
             status_code=200
         )
 
     else:
-        logging.info("Rejecting other web request")
+        logging.info("Rejecting other web request\n")
         return func.HttpResponse(
-            f"HTTP Method {req.method} not allowed",
+            f"HTTP Method {req.method} not allowed\n",
             status_code=400
         )
 
 if __name__ == "__main__":
     main()
+
